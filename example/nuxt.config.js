db0aaa7bfeb5a1991807716d9c36e2062f236d80
Sébastien Chopin <seb@nuxtjs.com>
2019-06-06 11:04:23 +0200

    chore: remove resourceHints to simplify

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -4,9 +4,6 @@ module.exports = {
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
-  render: {
-    resourceHints: false
-  },
   modules: [
     { handler: require('../') }
   ]
