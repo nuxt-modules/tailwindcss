3d4d73b8af1a479e66daaf6a6070542f2b01fead
pooya parsa <pooya@pi0.ir>
2020-03-26 16:10:17 +0100

    fix: lazy load module

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,10 +1,14 @@
 const { resolve } = require('path')
 
+function lazyLoadModule (opts) {
+  return require('../').call(this, opts)
+}
+
 module.exports = {
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
   modules: [
-    [require('../'), { exposeConfig: true }]
+    [lazyLoadModule, { exposeConfig: true }]
   ]
 }
