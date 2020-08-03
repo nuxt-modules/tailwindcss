6d4ddcc52b04f497381d89ec85f361dfaa5194ab
Sébastien Chopin <seb@nuxtjs.com>
2020-08-03 13:50:29 +0200

    chore: update default template

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -2,22 +2,10 @@
 ** TailwindCSS Configuration File
 **
 ** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+** Default: const defaultTheme = require('tailwindcss/defaultTheme')
 */
 module.exports = {
   theme: {},
   variants: {},
-  plugins: [],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    // We prefixed with `example/` here since we run `nuxt example/` and working dir is '../' for PurgeCSS
-    content: [
-      'example/components/**/*.vue',
-      'example/layouts/**/*.vue',
-      'example/pages/**/*.vue',
-      'example/plugins/**/*.js',
-      'example/nuxt.config.js'
-    ]
-  }
+  plugins: []
 }
