81feb24c6143675ebe32f9b34ada95ea53cc2ac3
fgiraud <hello@florent.dev>
2021-02-11 13:02:14 -0500

    fix: update ouput of default tailwind config

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  theme: {},
-  variants: {},
-  plugins: [],
-  purge: {
-    enabled: false,
-    content: [
-      'C:\\Users\\florent\\workdir\\tailwindcss-module\\example/components/**/*.{vue,js}',
-      'C:\\Users\\florent\\workdir\\tailwindcss-module\\example/layouts/**/*.vue',
-      'C:\\Users\\florent\\workdir\\tailwindcss-module\\example/pages/**/*.vue',
-      'C:\\Users\\florent\\workdir\\tailwindcss-module\\example/plugins/**/*.{js,ts}',
-      'C:\\Users\\florent\\workdir\\tailwindcss-module\\example/nuxt.config.{js,ts}'
-    ]
-  }
-}
+module.exports = {}
