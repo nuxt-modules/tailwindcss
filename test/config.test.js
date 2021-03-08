389b211e18dca70ceb3d105a6db27df8ecd86e4c
Sébastien Chopin <seb@nuxtjs.com>
2021-03-08 19:16:00 +0100

    chore: update test to use purge.content

--- test/config.test.js
+++ test/config.test.js
@@ -51,7 +51,7 @@ describe('config', () => {
   })
 
   test('merged the tailwind default config', () => {
-    expect(nuxt.options.build.postcss.plugins.tailwindcss.purge).toContain(`${rootDir}/nuxt.config.{js,ts}`)
-    expect(nuxt.options.build.postcss.plugins.tailwindcss.purge).toContain('content/**/*.md')
+    expect(nuxt.options.build.postcss.plugins.tailwindcss.purge.content).toContain(`${rootDir}/nuxt.config.{js,ts}`)
+    expect(nuxt.options.build.postcss.plugins.tailwindcss.purge.content).toContain('content/**/*.md')
   })
 })
