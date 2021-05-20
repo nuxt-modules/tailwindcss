8cf7a438cd331e9ba2c0fda5a25ff2f052bce63f
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-21 00:54:23 +0200

    :wrench: (docus) add docus config

---
+++ docs/docus.config.js
@@ -0,0 +1,18 @@
+export default {
+  title: "Nuxt TailwindCSS",
+  url: "https://tailwindcss.nuxtjs.org",
+  twitter: "nuxt_js",
+  github: "nuxt-community/tailwindcss-module",
+  theme: {
+    colors: {
+      primary: "#38b2ac",
+    },
+    header: {
+      logo: {
+        light: "/logo-light.svg",
+        dark: "/logo-dark.svg",
+      },
+      title: false,
+    },
+  },
+};
