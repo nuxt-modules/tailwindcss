21492b0eacc82fc0fe7c9a7629bac6af8a6f1f5f
chenly <chenly@gmail.com>
2022-04-22 23:10:10 +0800

    Update module.ts

--- src/module.ts
+++ src/module.ts
@@ -109,10 +109,11 @@ export default defineNuxtModule({
 
     // Add _tailwind config viewer endpoint
     if (nuxt.options.dev && moduleOptions.viewer) {
-      const path = '/_tailwind'
+      const path = '/_tailwind/'
+      const prefix = "/_tailwind"
       const createServer = await import('tailwind-config-viewer/server/index.js').then(r => r.default || r)
       const { withoutTrailingSlash } = await import('ufo')
-      const _viewerDevMiddleware = createServer({ tailwindConfigProvider: () => tailwindConfig }).asMiddleware()
+      const _viewerDevMiddleware = createServer({ tailwindConfigProvider: () => tailwindConfig, routerPrefix : prefix }).asMiddleware()
       const viewerDevMiddleware = (req, res) => {
         if (req.originalUrl === withoutTrailingSlash(path)) {
           res.writeHead(301, { Location: withTrailingSlash(req.originalUrl) })
