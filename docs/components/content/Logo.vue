bfa0ff49ac872c29a9d494d0f2bbe2dc2bab6c99
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-05-30 17:55:51 +0200

    docs: migrate to docus

---
+++ docs/components/content/Logo.vue
@@ -0,0 +1,4 @@
+<template>
+  <img src="/logo-light.svg" alt="logo" class="light-img">
+  <img src="/logo-dark.svg" alt="logo" class="dark-img">
+</template>
---
+++ docs/nuxt.config.ts
@@ -0,0 +1,26 @@
+import { defineNuxtConfig } from 'nuxt'
+import colors from 'tailwindcss/colors.js'
+
+export default defineNuxtConfig({
+  extends: ['./node_modules/@docus/docs-theme'],
+  github: {
+    owner: 'nuxt-community',
+    repo: 'tailwindcss-module',
+    branch: 'main'
+  },
+  modules: ['@docus/github', 'vue-plausible'],
+  plausible: {
+    domain: 'tailwindcss.nuxtjs.org'
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            primary: colors.sky
+          }
+        }
+      }
+    }
+  }
+})
