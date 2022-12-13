161b345c4cf2c45f34971190827cd5f177435891
Inesh Bose <56732164+ineshbose@users.noreply.github.com>
2022-12-13 15:59:33 +0000

    refactor: using join

--- src/module.ts
+++ src/module.ts
@@ -150,7 +150,7 @@ export default defineNuxtModule<ModuleOptions>({
     if (moduleOptions.exposeConfig) {
       const template = addTemplate({
         filename: 'tailwind.config.mjs',
-        getContents: () => `${Object.entries(resolvedConfig).map(([k, v]) => `export const ${k} = ${JSON.stringify(v, null, 2)}`).join('\n')}\nexport default {${Object.keys(resolvedConfig)}}`
+        getContents: () => `${Object.entries(resolvedConfig).map(([k, v]) => `export const ${k} = ${JSON.stringify(v, null, 2)}`).join('\n')}\nexport default { ${Object.keys(resolvedConfig).join(', ')} }`
       })
       addTemplate({
         filename: 'tailwind.config.d.ts',
