4aa9f3d3c4748ae2db47e3ac3e0a1eb17c7d9491
Sébastien Chopin <seb@nuxtjs.com>
2023-01-25 23:35:02 +0100

    chore: add comment

--- src/module.ts
+++ src/module.ts
@@ -141,6 +141,7 @@ export default defineNuxtModule<ModuleOptions>({
     // Write cjs version of config to support vscode extension
     const resolveConfig: any = await import('tailwindcss/resolveConfig.js').then(r => r.default || r)
     const resolvedConfig = resolveConfig(tailwindConfig)
+    // Avoid creating null plugins for intelisense
     resolvedConfig.plugins = []
     addTemplate({
       filename: 'tailwind.config.cjs',
