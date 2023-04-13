237d4ca213071077db0e9e6cdd97873f3f00e95c
Benjamin Canac <canacb1@gmail.com>
2023-04-13 09:52:46 +0200

    chore(module): call hook before resolving config

--- src/module.ts
+++ src/module.ts
@@ -153,6 +153,9 @@ export default defineNuxtModule<ModuleOptions>({
         tailwindConfig = defuArrayFn(_tailwindConfig, tailwindConfig)
       }
     }
+    
+    // Allow extending tailwindcss config by other modules
+    await nuxt.callHook('tailwindcss:config', tailwindConfig)
 
     // Write cjs version of config to support vscode extension
     const resolveConfig = await import('tailwindcss/resolveConfig.js').then(r => r.default || r)
@@ -173,9 +176,6 @@ export default defineNuxtModule<ModuleOptions>({
     if (moduleOptions.exposeConfig) {
       createTemplates(resolvedConfig, moduleOptions.exposeLevel, nuxt)
     }
-
-    // Allow extending tailwindcss config by other modules
-    await nuxt.callHook('tailwindcss:config', tailwindConfig)
 
     // Compute tailwindConfig hash
     tailwindConfig._hash = String(Date.now())
