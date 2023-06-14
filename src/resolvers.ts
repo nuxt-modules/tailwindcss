eb256e39033c2cac7efe94b8d38511fe029a618d
Inesh Bose <2504266b@student.gla.ac.uk>
2023-06-14 18:42:45 +0100

    chore: condense content paths

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -29,10 +29,8 @@ export const resolveContentPaths = (srcDir: string) => ([
   `${srcDir}/composables/**/*.{js,ts}`,
   `${srcDir}/plugins/**/*.{js,ts}`,
   `${srcDir}/utils/**/*.{js,ts}`,
-  `${srcDir}/App.{js,ts,vue}`,
-  `${srcDir}/app.{js,ts,vue}`,
-  `${srcDir}/Error.{js,ts,vue}`,
-  `${srcDir}/error.{js,ts,vue}`,
+  `${srcDir}/{App,app}.{js,ts,vue}`,
+  `${srcDir}/{Error,error}.{js,ts,vue}`,
   `${srcDir}/app.config.{js,ts}`
 ])
 
