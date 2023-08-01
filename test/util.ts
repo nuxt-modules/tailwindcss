23233be9dd6ffb26811f7a38f267c1d83bef4750
Inesh Bose <2504266b@student.gla.ac.uk>
2023-08-01 22:29:58 +0530

    fix(ci): attempt to fix test

--- test/util.ts
+++ test/util.ts
@@ -1,7 +1,8 @@
 import { fileURLToPath } from 'node:url'
 import { setup } from '@nuxt/test-utils'
+import { joinURL } from 'ufo'
 
-export const r = (s: string = '') => fileURLToPath(new URL('./fixture/basic/' + s, import.meta.url))
+export const r = (s: string = '') => fileURLToPath(new URL(joinURL('./fixture/basic', s), import.meta.url))
 
 export const setupNuxtTailwind = (tailwindcss = {}) => {
   return setup({
