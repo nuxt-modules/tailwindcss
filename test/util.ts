43234bef61259e841a2e68c64c51cc3406b12347
Inesh Bose <2504266b@student.gla.ac.uk>
2023-08-01 23:08:58 +0530

    chore: revert change

--- test/util.ts
+++ test/util.ts
@@ -1,8 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { setup } from '@nuxt/test-utils'
-import { joinURL } from 'ufo'
 
-export const r = (s: string = '') => fileURLToPath(new URL(joinURL('./fixture/basic', s), import.meta.url))
+export const r = (s: string = '') => fileURLToPath(new URL('./fixture/basic/' + s, import.meta.url))
 
 export const setupNuxtTailwind = (tailwindcss = {}) => {
   return setup({
