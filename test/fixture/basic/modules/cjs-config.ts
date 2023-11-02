f7096aa76dd442263b2bad9ac07b2d4be9cc5548
Inesh Bose <56732164+ineshbose@users.noreply.github.com>
2023-11-02 13:28:18 +0000

    feat: more adaptive content paths (#744)
    * chore: initial
    
    * chore: extensions
    
    * chore: update
    
    * chore: update test
    
    * chore: add conditions for composables and utils
    
    * chore: update
    
    * chore: update tests
    
    * chore: finish tests

--- test/fixture/basic/modules/cjs-config.ts
+++ test/fixture/basic/modules/cjs-config.ts
@@ -1,10 +1,12 @@
-import { defineNuxtModule, addTemplate } from '@nuxt/kit'
+import { defineNuxtModule, addTemplate, logger } from '@nuxt/kit'
 
 export default defineNuxtModule({
   setup (_options, nuxt) {
+    // logger.info('Creating test-tailwind.config.cjs...')
+
     nuxt.hook('tailwindcss:resolvedConfig', (config) => {
       addTemplate({
-        filename: 'tailwind.config.cjs', // gets prepended by .nuxt/
+        filename: 'test-tailwind.config.cjs', // gets prepended by .nuxt/
         getContents: () => `module.exports = ${JSON.stringify(config, null, 2)}`,
         write: true
       })
