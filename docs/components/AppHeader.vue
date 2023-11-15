e1bb61539678104c0fc5e1d819cabac92a9c6342
Sébastien Chopin <seb@nuxt.com>
2023-11-15 16:19:33 +0100

    docs: move to UI Pro

---
+++ docs/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { NavItem } from '@nuxt/content/dist/runtime/types'
+
+const navigation = inject<NavItem[]>('navigation', [])
+
+const { header } = useAppConfig()
+</script>
+
+<template>
+  <UHeader>
+    <template #logo>
+      <Logo class="h-6" />
+    </template>
+
+    <template v-if="header?.search" #center>
+      <UDocsSearchButton class="hidden lg:flex" />
+    </template>
+
+    <template #right>
+      <UDocsSearchButton v-if="header?.search" :label="null" class="lg:hidden" />
+
+      <UColorModeButton v-if="header?.colorMode" />
+
+      <template v-if="header?.links">
+        <UButton
+          v-for="(link, index) of header.links"
+          :key="index"
+          v-bind="{ color: 'gray', variant: 'ghost', ...link }"
+        />
+      </template>
+    </template>
+
+    <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" />
+    </template>
+  </UHeader>
+</template>
---
+++ docs/pages/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { joinURL } from 'ufo'
+
+const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+const { url } = useSiteConfig()
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description,
+  ogImage: joinURL(url, '/social-card.png'),
+  twitterImage: joinURL(url, '/social-card.png')
+})
+</script>
+
+<template>
+  <div>
+    <ULandingHero v-if="page.hero" v-bind="page.hero">
+      <template #title>
+        <MDC :value="page.hero.title" />
+      </template>
+
+      <MDC :value="page.hero.code" tag="pre" class="prose prose-primary dark:prose-invert max-w-none" />
+    </ULandingHero>
+
+    <ULandingSection :title="page.features.title">
+      <UPageGrid>
+        <ULandingCard v-for="(item, index) of page.features.items" :key="index" v-bind="item" />
+      </UPageGrid>
+    </ULandingSection>
+  </div>
+</template>
