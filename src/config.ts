99cc0f634e686db55c0f9cd6366440f1301d94bb
Inesh Bose <dev@inesh.xyz>
2025-04-24 10:35:18 +0000

    fix: attempt with unctx

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,13 @@
-import { fileURLToPath } from 'node:url'
-import { resolve } from 'pathe'
 import { createDefineConfig } from 'c12'
+import { getContext } from 'unctx'
 import { tryUseNuxt, requireModule } from '@nuxt/kit'
 import type { Config } from 'tailwindcss'
 
-const pathToThisFile = resolve(fileURLToPath(import.meta.url))
-const pathPassedToNode = resolve(process.argv[1])
-const isMainFile = pathToThisFile.includes(pathPassedToNode)
+export const ctx = getContext<boolean>('tw-config-ctx')
 
 const _defineConfig = createDefineConfig<Partial<Config>>()
 export const defineConfig: typeof _defineConfig = (config) => {
   const isNuxt = !!tryUseNuxt()
-  return isNuxt ? config : isMainFile ? requireModule('.nuxt/tailwind/postcss.mjs') : config
+  return isNuxt || ctx.tryUse() ? config : requireModule('.nuxt/tailwind/postcss.mjs')
 }
 export default defineConfig
